684ec1a1d97321f050a639f32953313d596a72b3
richard lu <luxh93@gmail.com>
2020-11-01 14:14:40 -0500

    add python 3 support

--- spotempo.py
+++ spotempo.py
@@ -17,7 +17,7 @@ def main():
         "Authorization": "Bearer {}".format(spot_token)
     }
 
-    name = raw_input("Name of playlist you would like to arrange by tempo: ").strip()
+    name = input("Name of playlist you would like to arrange by tempo: ").strip()
 
     # Get list of top 50 playlists
     playlists_req = requests.get("https://api.spotify.com/v1/users/{}/playlists?limit=50".format(username),
@@ -31,31 +31,31 @@ def main():
             found = True
             playlist_id = pl["id"]
             num_of_songs = pl["tracks"]["total"]
-            print "Playlist found. Number of tracks: " + str(num_of_songs)
+            print("Playlist found. Number of tracks: " + str(num_of_songs))
             break
     if not found:
-        print "Unable to find playlist: make sure it is in your first 50 playlists and you spell the name correctly"
+        print("Unable to find playlist: make sure it is in your first 50 playlists and you spell the name correctly")
         exit()
 
     # User chooses if they want new play list in ascending or descending order
-    arrangement = raw_input("Would you like to arrange this playlist in\n"
-                            "1) Ascending order (Low to high tempos)\n"
-                            "2) Descending order (High to low tempos)\n"
-                            "Enter either 1 or 2: ").strip()
+    arrangement = input("Would you like to arrange this playlist in\n"
+                        "1) Ascending order (Low to high tempos)\n"
+                        "2) Descending order (High to low tempos)\n"
+                        "Enter either 1 or 2: ").strip()
 
     if arrangement != str(1) and arrangement != str(2):
         while arrangement != str(1) and arrangement != str(2):
-            arrangement = raw_input("Please enter a 1 or 2: ").strip()
+            arrangement = input("Please enter a 1 or 2: ").strip()
 
     # Have user choose name of new playlist
-    new_name = raw_input("Name of new playlist: ").strip()
+    new_name = input("Name of new playlist: ").strip()
 
-    print "Getting songs and tempos... "
+    print("Getting songs and tempos... ")
 
     # Get each song and tempo in sets of 100 (because of API limits)
     tempos = []
     noq_round = int(math.ceil(num_of_songs / 100.0))
-    for i in xrange(0, noq_round):
+    for i in range(0, noq_round):
         # Get all the tracks in the playlist
         tr_names = []
         tr_ids = []
@@ -70,11 +70,11 @@ def main():
         tempo_req = requests.get("https://api.spotify.com/v1/audio-features?ids="+queryparam[0:-1], headers=header)
         trjson = tempo_req.json()["audio_features"]
         # Get tempo for each song and add the name, tempo, and id of the song to tempos
-        for t in xrange(0, len(trjson)):
+        for t in range(0, len(trjson)):
             try:
                 tempos.append((tr_names[t], trjson[t]["tempo"], tr_ids[t]))
             except TypeError:  # When a song doesn't exist
-                print "Error with a song"
+                print("Error with a song")
 
     # Sort the list of songs by tempo
     if arrangement == str(1):
@@ -98,16 +98,16 @@ def main():
     enter = requests.post("https://api.spotify.com/v1/users/{}/playlists".format(username), data=json.dumps(data),
                           headers=headermake)
     if str(enter)[11:14] == str(201) or str(enter)[11:14] == str(200):  # If response was 200 or 201 (successful)
-        print "Successfully made playlist"
+        print("Successfully made playlist")
         new_plid = enter.json()["id"]
     else:
-        print "Unable to make playlist"
-        print enter
+        print("Unable to make playlist")
+        print(enter)
         exit()
 
     # Add songs to playlist in sets of 100
     noq_round = int(math.ceil(len(tempos) / 100.0))
-    for i in xrange(0, noq_round):
+    for i in range(0, noq_round):
         uris_list = []
         # For each song in the list of songs sorted by their tempo
         for song in tempos[i * 100:(i * 100) + 100]:
@@ -119,12 +119,12 @@ def main():
                                      headers=headermake)
         # If response isn't successful
         if str(adding_songs)[11:14] != str(201) and str(adding_songs)[11:14] != str(200):
-            print "Error adding songs"
-            print adding_songs
+            print("Error adding songs")
+            print(adding_songs)
             exit()
 
     # When everything is complete and successful
-    print "Success! All songs have been added to new playlist"
+    print("Success! All songs have been added to new playlist")
 
 
 def spotify_authenticate(username, scope, client_id, client_secret):
